2523bbd10210e570ab4cf58a316bcdc43558274b
Andrew <62702514+AndrewMommers@users.noreply.github.com>
2023-03-25 16:17:26 +1100

    minecraft bot changes

--- Bots/minecraft.js
+++ Bots/minecraft.js
@@ -2,13 +2,14 @@ const Gamedig = require('gamedig')
 const Discord = require('discord.js')
 const client = new Discord.Client({ intents: ['GUILDS'] })
 
+var page = 0
 var status = {}
 
 function Start(token, game) {
     client.login(token)
 
     client.on('ready', async () => {
-        console.log(`Minecraft Logged in as ${client.user.tag}!`);
+        console.log(`Logged in as ${client.user.tag}!`);
         client.user.setActivity(`Collecting Data...`, { type: 'WATCHING' })
         client.user.setStatus('idle')
 
@@ -18,31 +19,39 @@ function Start(token, game) {
 
             Gamedig.query({
                 type: 'minecraft',
-                host: '192.168.1.55',
+                host: '139.99.133.18',
                 maxAttempts: 3,
                 socketTimeout: 3000,
                 port: 25565
             }).then((body) => {
                 status = body
-                if (body.raw.numplayers > 0) {
-                    client.user.setActivity(`${body.raw.numplayers} / ${body.maxplayers} Players / Ping ${body.ping} `,  { type: 'WATCHING' })
-                    client.user.setStatus('online')
+                client.user.setStatus('online')
+                if (page === 0) {(body.raw.vanilla.raw.players.online > 0)
+                    page = 1
+                    client.user.setActivity(`${body.raw.vanilla.raw.players.online} / ${body.raw.vanilla.raw.players.max} Players`, { type: 'WATCHING' })
+                    return
                 }
-                else {
-                    client.user.setActivity(`No Players Online / Ping ${body.ping}`, { type: 'WATCHING' })
-                    client.user.setStatus('idle')
+                if (page === 1 ) {
+                    page = 0
+                    client.user.setActivity(`${body.ping} Ping`, {type: 'WATCHING'})
+                    return
                 }
-            }).catch((error) => {
+            }).catch(err => {
                 status = {}
+
+                console.log(err)
+
                 client.user.setActivity(`Server Offline`, { type: 'WATCHING' })
                 client.user.setStatus('dnd')
             })
 
         }
         setInterval(refresh, 10000)
-        
+
     })
 }
+
+
 
 module.exports = {
     Start: Start
