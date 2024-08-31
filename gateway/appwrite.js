afbb38fe79d428c701a048112920c070669bdb65
Andrew <62702514+AndrewMommers@users.noreply.github.com>
2024-08-31 16:38:35 +1000

    Patch Update
    Ticket System
    audits

--- gateway/appwrite.js
+++ gateway/appwrite.js
@@ -2,19 +2,27 @@ require('dotenv').config();
 const sdk = require('node-appwrite');
 const moment = require('moment'); // For date manipulation
 
-const client = new sdk.Client();
-const databases = new sdk.Databases(client);
-
 const config = require('../config.json');
 
 // Initialize the Appwrite client
-client
-    .setEndpoint(config.appwrite.endpoint) // Appwrite endpoint
-    .setProject(config.appwrite.projectId) // Appwrite project ID
-    .setKey(config.appwrite.apiKey); // Appwrite API key
+const getAppwriteClient = () => {
+    const client = new sdk.Client();
+
+    client
+        .setEndpoint(config.appwrite.endpoint) // Appwrite endpoint
+        .setProject(config.appwrite.projectId) // Appwrite project ID
+        .setKey(config.appwrite.apiKey); // Appwrite API key
+
+    return {
+        client,
+        databases: new sdk.Databases(client),
+    };
+};
 
 // Define the function to add a user to the database
 const addUserToDatabase = async (user) => {
+    const { databases } = getAppwriteClient();
+
     try {
         // Create a document in the usersCollectionId
         const response = await databases.createDocument(
@@ -32,6 +40,8 @@ const addUserToDatabase = async (user) => {
 
 // Define the function to get a user by discordUserId
 const getUserByDiscordId = async (discordUserId) => {
+    const { databases } = getAppwriteClient();
+
     try {
         const response = await databases.listDocuments(
             config.appwrite.discordDatabase.discordDatabaseId, // Discord Database ID
@@ -51,6 +61,8 @@ const getUserByDiscordId = async (discordUserId) => {
 
 // Define the function to update the user's status
 const updateUserStatus = async (discordUserId, updateData) => {
+    const { databases } = getAppwriteClient();
+
     try {
         const user = await getUserByDiscordId(discordUserId);
         if (!user) {
@@ -71,39 +83,35 @@ const updateUserStatus = async (discordUserId, updateData) => {
 
 // Function to add all current Discord users who joined before today
 const addAllCurrentUsers = async (guild) => {
-    try {
-        const today = moment().startOf('day');
-        const members = await guild.members.fetch();
-        
-        for (const [_, member] of members) {
-            const joinedAt = moment(member.joinedAt);
-            if (joinedAt.isBefore(today)) {
-                const user = {
-                    discordUserId: member.id,
-                    username: member.user.tag,
-                    JoinedAt: joinedAt.toISOString(),
-                    verifiedStatus: false, // Default value
-                    verificationDate: null,
-                    lastActive: new Date().toISOString(),
-                    roles: member.roles.cache.map(role => role.id),
-                    warnings: 0,
-                    bans: 0,
-                    lastAction: null,
-                    notes: '',
-                    ticketIds: [],
-                    discordCreation: member.user.createdAt.toISOString(),
-                };
+    const today = moment().startOf('day');
+    const members = await guild.members.fetch();
 
-                try {
-                    await addUserToDatabase(user);
-                    console.log(`User ${member.user.tag} added to the database.`);
-                } catch (error) {
-                    console.error(`Error adding user ${member.user.tag} to database: ${error.message}`);
-                }
+    for (const [_, member] of members) {
+        const joinedAt = moment(member.joinedAt);
+        if (joinedAt.isBefore(today)) {
+            const user = {
+                discordUserId: member.id,
+                username: member.user.tag,
+                JoinedAt: joinedAt.toISOString(),
+                verifiedStatus: false, // Default value
+                verificationDate: null,
+                lastActive: new Date().toISOString(),
+                roles: member.roles.cache.map(role => role.id),
+                warnings: 0,
+                bans: 0,
+                lastAction: null,
+                notes: '',
+                ticketIds: [],
+                discordCreation: member.user.createdAt.toISOString(),
+            };
+
+            try {
+                await addUserToDatabase(user);
+                console.log(`User ${member.user.tag} added to the database.`);
+            } catch (error) {
+                console.error(`Error adding user ${member.user.tag} to database: ${error.message}`);
             }
         }
-    } catch (error) {
-        console.error('Error adding all current users to Appwrite:', error);
     }
 };
 
@@ -121,6 +129,8 @@ const startDatabaseUpdater = (guild) => {
 
 // Function to add a log entry to the audit collection
 const addLogToAuditCollection = async (log) => {
+    const { databases } = getAppwriteClient();
+
     try {
         const response = await databases.createDocument(
             config.appwrite.discordDatabase.discordDatabaseId,
@@ -139,4 +149,11 @@ const addLogToAuditCollection = async (log) => {
     }
 };
 
-module.exports = { addUserToDatabase, getUserByDiscordId, updateUserStatus, startDatabaseUpdater, addLogToAuditCollection };
+module.exports = { 
+    getAppwriteClient, 
+    addUserToDatabase, 
+    getUserByDiscordId, 
+    updateUserStatus, 
+    startDatabaseUpdater, 
+    addLogToAuditCollection 
+};
