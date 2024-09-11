c4316e542037072e29d0590878281e7560a42934
Mommers Co <149940328+MommersCo@users.noreply.github.com>
2024-09-10 19:30:31 -0700

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,2 +1,9 @@
-@ECHO OFF
+@echo off
+:loop
+echo Starting the bot...
 node --trace-warnings client/client.js
+echo Bot crashed with exit code %ERRORLEVEL%.
+echo Restarting bot...
+timeout /t 5
+goto loop
+
