70c96506e6860dd7656eb83337eb7fb758648435
Andrew <62702514+AndrewMommers@users.noreply.github.com>
2025-03-23 04:30:00 +1100

    Update userService.ts

--- core/userService.ts
+++ core/userService.ts
@@ -1,35 +1,13 @@
 import Database from './database';
 import Logger from './logger';
-import { Client, GuildMember } from 'discord.js';
+import { GuildMember } from 'discord.js';
 
 class UserService {
     // Function to check if the user exists and add them if not
     async ensureUserExists(member: GuildMember) {
         try {
-            const existingUser = await Database.checkExistingUser(member.id);
-
-            if (!existingUser) {
-                // Create the user object to store in the database
-                const newUser = {
-                    discordUserId: member.id,
-                    username: member.user.username,
-                    JoinedAt: member.joinedAt?.toISOString() ?? '',
-                    verifiedStatus: false,
-                    verificationDate: null,
-                    lastActive: member.user.lastMessage?.createdAt?.toISOString() ?? member.joinedAt?.toISOString() ?? new Date().toISOString(),
-                    roles: member.roles.cache.map((role) => role.name),
-                    warnings: 0,
-                    bans: 0,
-                    lastAction: null,
-                    notes: '',
-                    ticketIds: [],
-                    discordCreation: member.user.createdAt.toISOString(),
-                };
-
-                // Add the new user to the database
-                await Database.addUserToDatabase(newUser);
-                Logger.info(`Added user ${member.user.tag} (ID: ${member.id}) to the database.`);
-            }
+            // Use the method from the Database class to check and add the user if necessary
+            await Database.ensureUserExists(member.id, member);
         } catch (error: unknown) {
             if (error instanceof Error) {
                 Logger.error(`Error checking or adding user ${member.user.tag}: ${error.message}`);
