274dafe71dbd4bf4196127bcf60053fb5e9d3dd6
Andrew <62702514+AndrewMommers@users.noreply.github.com>
2025-11-23 20:08:09 +1100

    Update database.ts

--- core/database.ts
+++ core/database.ts
@@ -67,6 +67,13 @@ class Database {
                 host: config.database.host,
                 dialect: config.database.type as any,
                 logging: (sql, timing) => Logger.database(`${sql}${timing ? ` (${timing} ms)` : ''}`),
+
+                dialectOptions: {
+                    ssl: {
+                        require: true,
+                        rejectedUnauthorized: false
+                    }
+                }
             }
         );
 
